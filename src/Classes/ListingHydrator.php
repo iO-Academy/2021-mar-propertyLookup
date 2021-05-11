954a3d3a009f1f27901bd39b56650161e28439b8
Maria Gusova <masha.gu@gmail.com>
2021-05-11 13:31:38 +0100

    add methods

--- src/Classes/ListingHydrator.php
+++ src/Classes/ListingHydrator.php
@@ -3,8 +3,30 @@
 
 namespace ListingsApp\Classes;
 
+/**
+ * Class ListingHydrator
+ * @package ListingsApp\Classes
+ */
 
 class ListingHydrator
 {
 
+    public static function getForSaleListings(PDO $db)
+    {
+        $query = $db->prepare('SELECT * FROM `listings` WHERE `type` = 1;');
+        $query->execute();
+        $query->setFetchMode(PDO::FETCH_CLASS, Listing::class);
+        return $query->fetchAll();
+    }
+
+    public static function getForLetListings(PDO $db)
+    {
+        $query = $db->prepare('SELECT * FROM `listings` WHERE `type` = 2;');
+        $query->execute();
+        $query->setFetchMode(PDO::FETCH_CLASS, Listing::class);
+        return $query->fetchAll();
+    }
 }
+
+
+
