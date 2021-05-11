8c5f56c5b6bd91fe69c9f1f98cdb72680546f2cb
Isabelle Cochrane <isabelle_cochrane@hotmail.co.uk>
2021-05-11 15:05:27 +0100

    populateDb query

--- index.php
+++ index.php
@@ -2,18 +2,19 @@
 use ListingsApp\Classes\ListingAPI;
 use ListingsApp\Classes\DBPopulator;
 require_once 'vendor/autoload.php';
-
+$db = new PDO('mysql:host=db;dbname=property-feed', 'root', 'password');
+var_dump($db);
 $trial = new ListingAPI();
-var_dump($trial->getStatusesFromApi());
-var_dump($trial->getTypesFromApi());
-var_dump($trial->getListingsFromApi());
+//var_dump($trial->getStatusesFromApi());
+//var_dump($trial->getTypesFromApi());
+//var_dump($trial->getListingsFromApi());
 
-$test = new DBPopulator();
+
 
 $api = new ListingAPI();
 echo '<pre>';
 var_dump($api->getTypesFromApi());
 echo '</pre>';
-
+$test = new DBPopulator($api, $db);
 
 
--- src/Classes/DBPopulator.php
+++ src/Classes/DBPopulator.php
@@ -3,7 +3,12 @@ namespace ListingsApp\Classes;
 class DBPopulator {
     private object $listingAPI;
     private $dbConnection;
-    public function __construct($listingAPI) {
+    public function __construct($listingAPI, $db) {
         $this->listingAPI = $listingAPI;
+        $this->dbConnection = $db;
+    }
+
+    public function populateDBListingsTable() {
+        $query = $this->dbConnection->prepare('INSERT INTO `property-feed` (`AGENT_REF`, `ADDRESS_1`, `ADDRESS_2`, `TOWN`, `POSTCODE`, `DESCRIPTION`, `BEDROOMS`, `PRICE`, `IMAGE`, `TYPE`, `STATUS`) VALUES ;');
     }
 }
