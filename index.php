1742aa1540b402e1885eb38f877db8c5a343ffb6
Imy <imypullan@gmail.com>
2021-05-11 14:30:34 +0100

    dbpop class now included in index.php

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <?php
-use ListingsApp\Classes\Api;
+use ListingsApp\Classes\ListingAPI;
+use ListingsApp\Classes\DBPopulator;
 require_once 'vendor/autoload.php';
 
-$trial = new Api();
-var_dump($trial->getStatusesFromApi());
-var_dump($trial->getTypesFromApi());
-var_dump($trial->getListingsFromApi());
+$test = new DBPopulator();
+
+$api = new ListingAPI();
+echo '<pre>';
+var_dump($api->getTypesFromApi());
+echo '</pre>';
 
 
