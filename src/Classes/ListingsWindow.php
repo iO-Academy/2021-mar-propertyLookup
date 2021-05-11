9ba86fd5c8fcb4660f178f417d9b5b94360e7704
Maria Gusova <masha.gu@gmail.com>
2021-05-11 16:27:58 +0100

    add ListingsWindow

---
+++ src/Classes/ListingsWindow.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace ListingsApp\Classes;
+
+
+class ListingsWindow
+{
+    public static function displayListings() {
+        //if get is set to sales we call the getForSaleListings()
+        // this will return index.php?type=sales
+        // if get is set to let we call the getForLetListings()
+        // this will return index.php?type=lettings
+        // else get all listings
+        // return all
+    }
+}
