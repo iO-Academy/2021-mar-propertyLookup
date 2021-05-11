60f820c4b3f6c9ccd72db548b451af5be16cbc5a
Isabelle Cochrane <isabelle_cochrane@hotmail.co.uk>
2021-05-11 12:01:27 +0100

    dryer Api class

--- src/Classes/Api.php
+++ src/Classes/Api.php
@@ -10,27 +10,32 @@ class Api {
     public function __construct() {
         $this->request = curl_init();
     }
-    public function getListingsFromApi(): array {
 
-        curl_setopt($this->request, CURLOPT_URL, "https://dev.io-academy.uk/resources/property-feed/properties.json");
+    private function decodeJson() {
+        json_decode(curl_exec($this->request));
+    }
+
+    private function curlRequest($endpoint) {
+        curl_setopt($this->request, CURLOPT_URL, "https://dev.io-academy.uk/resources/property-feed/$endpoint.json");
         curl_setopt($this->request, CURLOPT_RETURNTRANSFER, 1);
-        $this->allListings = json_decode(curl_exec($this->request));
+    }
 
+    public function getListingsFromApi(): array {
+        $this->curlRequest('properties');
+        $this->allListings = json_decode(curl_exec($this->request));
         return $this->allListings;
     }
 
     public function getTypesFromApi(): array {
 
-        curl_setopt($this->request, CURLOPT_URL, "https://dev.io-academy.uk/resources/property-feed/types.json");
-        curl_setopt($this->request, CURLOPT_RETURNTRANSFER, 1);
+        $this->curlRequest('types');
         $this->lettingTypes = json_decode(curl_exec($this->request));
         return $this->lettingTypes;
     }
 
     public function getStatusesFromApi(): array {
 
-        curl_setopt($this->request, CURLOPT_URL, "https://dev.io-academy.uk/resources/property-feed/statuses.json");
-        curl_setopt($this->request, CURLOPT_RETURNTRANSFER, 1);
+        $this->curlRequest('statuses');
         $this->lettingStatuses= json_decode(curl_exec($this->request));
         return $this->lettingStatuses;
     }
