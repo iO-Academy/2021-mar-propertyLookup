aaa5da8f028fab2f62eee0f4ec81b824b15d3c19
Imy <imypullan@gmail.com>
2021-05-11 15:49:40 +0100

    populateDBListingsTable working

--- index.php
+++ index.php
@@ -2,19 +2,12 @@
 use ListingsApp\Classes\ListingAPI;
 use ListingsApp\Classes\DBPopulator;
 require_once 'vendor/autoload.php';
-$db = new PDO('mysql:host=db;dbname=property-feed', 'root', 'password');
-var_dump($db);
-$trial = new ListingAPI();
-//var_dump($trial->getStatusesFromApi());
-//var_dump($trial->getTypesFromApi());
-//var_dump($trial->getListingsFromApi());
+$db = new PDO('mysql:host=db;dbname=listings-feed', 'root', 'password');
+$listingAPI = new ListingAPI();
+$dbPopulator = new DBPopulator($listingAPI, $db);
 
 
-
-$api = new ListingAPI();
 echo '<pre>';
-var_dump($api->getTypesFromApi());
+$dbPopulator->populateDBListingsTable();
 echo '</pre>';
-$test = new DBPopulator($api, $db);
-
 
--- src/Classes/DBPopulator.php
+++ src/Classes/DBPopulator.php
@@ -3,12 +3,28 @@ namespace ListingsApp\Classes;
 class DBPopulator {
     private object $listingAPI;
     private $dbConnection;
+    private $listings;
     public function __construct($listingAPI, $db) {
         $this->listingAPI = $listingAPI;
         $this->dbConnection = $db;
     }
 
     public function populateDBListingsTable() {
-        $query = $this->dbConnection->prepare('INSERT INTO `property-feed` (`AGENT_REF`, `ADDRESS_1`, `ADDRESS_2`, `TOWN`, `POSTCODE`, `DESCRIPTION`, `BEDROOMS`, `PRICE`, `IMAGE`, `TYPE`, `STATUS`) VALUES ;');
+        $this->listings = $this->listingAPI->getListingsFromApi();
+        foreach($this->listings as $listing) {
+            $query = $this->dbConnection->prepare('INSERT INTO `listings` (`AGENT_REF`, `ADDRESS_1`, `ADDRESS_2`, `TOWN`, `POSTCODE`, `DESCRIPTION`, `BEDROOMS`, `PRICE`, `IMAGE`, `TYPE`, `STATUS`) VALUES (:agent_ref, :address_1, :address_2, :town, :postcode, :description, :bedrooms, :price, :image, :type, :status);');
+            $query->bindParam(':agent_ref', $listing->AGENT_REF);
+            $query->bindParam(':address_1', $listing->ADDRESS_1);
+            $query->bindParam(':address_2', $listing->ADDRESS_2);
+            $query->bindParam(':town', $listing->TOWN);
+            $query->bindParam(':postcode', $listing->POSTCODE);
+            $query->bindParam(':description', $listing->DESCRIPTION);
+            $query->bindParam(':bedrooms', $listing->BEDROOMS);
+            $query->bindParam(':price', $listing->PRICE);
+            $query->bindParam(':image', $listing->IMAGE);
+            $query->bindParam(':type', $listing->TYPE);
+            $query->bindParam(':status', $listing->STATUS);
+            $query->execute();
+        }
     }
 }
