73644beb8489ee4d1842b9d000cb6496430aed1c
Isabelle Cochrane <isabelle_cochrane@hotmail.co.uk>
2021-05-11 11:17:28 +0100

    getFromApi methods returning arrays

--- src/Classes/Api.php
+++ src/Classes/Api.php
@@ -2,20 +2,30 @@
 namespace ListingsApp\Classes;
 
 class Api {
-    public static function getListingsFromApi(): string {
+    public static function getListingsFromApi(): array {
         $request = curl_init();
         curl_setopt($request, CURLOPT_URL, "https://dev.io-academy.uk/resources/property-feed/properties.json");
         curl_setopt($request, CURLOPT_RETURNTRANSFER, 1);
-        $output = curl_exec($request);
+        $output = json_decode(curl_exec($request));
         curl_close($request);
         return $output;
     }
 
-    private static function getTypesFromApi(): array {
-
+    public static function getTypesFromApi(): array {
+        $request = curl_init();
+        curl_setopt($request, CURLOPT_URL, "https://dev.io-academy.uk/resources/property-feed/types.json");
+        curl_setopt($request, CURLOPT_RETURNTRANSFER, 1);
+        $output = json_decode(curl_exec($request));
+        curl_close($request);
+        return $output;
     }
 
-    private static function getStatusesFromApi(): array {
-
+    public static function getStatusesFromApi(): array {
+        $request = curl_init();
+        curl_setopt($request, CURLOPT_URL, "https://dev.io-academy.uk/resources/property-feed//statuses.json");
+        curl_setopt($request, CURLOPT_RETURNTRANSFER, 1);
+        $output = json_decode(curl_exec($request));
+        curl_close($request);
+        return $output;
     }
 }
