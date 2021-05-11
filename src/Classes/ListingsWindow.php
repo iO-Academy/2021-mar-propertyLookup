7a9402a0ba86e32808c87afa4cbbfb8abff43b95
Maria Gusova <masha.gu@gmail.com>
2021-05-11 17:31:19 +0100

    add function displayListings

--- src/Classes/ListingsWindow.php
+++ src/Classes/ListingsWindow.php
@@ -2,17 +2,23 @@
 
 
 namespace ListingsApp\Classes;
+use PDO;
 
+//<a href="index.php?type=sales">Sales Listings</a>
 
 class ListingsWindow
 {
     public static function displayListings() {
-        //if get is set to sales we call the getForSaleListings()
-        // this will return index.php?type=sales
-        // if get is set to let we call the getForLetListings()
-        // this will return index.php?type=lettings
-        // else get all listings
-        // return all
-
+        if (isset($_GET)){
+            if ($_GET['type']==='sales') {
+                return ListingHydrator::getForSaleListings($db);
+            }
+                if ($_GET['type']==='lettings') {
+                    return ListingHydrator::getForLetListings($db);
+                }
+                if($_GET['type']==='all') {
+                    return ListingHydrator::getAllListings();
+                }
+        } return ListingHydrator::getAllListings();
     }
 }
