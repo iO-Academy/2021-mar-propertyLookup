b806852d04ac32b177734f54240089fcf4a537e8
Isabelle Cochrane <isabelle_cochrane@hotmail.co.uk>
2021-05-11 11:08:47 +0100

    getListingsFromApi curl

--- src/Classes/Api.php
+++ src/Classes/Api.php
@@ -2,13 +2,13 @@
 namespace ListingsApp\Classes;
 
 class Api {
-    public static function getListingsFromApi(): array {
-        $request = curl_init("https://dev.maydenacademy.co.uk/resources/property-feed/properties.json");
-//        curl_setopt($request, CURLOPT_URL, "https://dev.maydenacademy.co.uk/resources/property-feed/properties.json");
+    public static function getListingsFromApi(): string {
+        $request = curl_init();
+        curl_setopt($request, CURLOPT_URL, "https://dev.io-academy.uk/resources/property-feed/properties.json");
         curl_setopt($request, CURLOPT_RETURNTRANSFER, 1);
         $output = curl_exec($request);
-        var_dump($output);
         curl_close($request);
+        return $output;
     }
 
     private static function getTypesFromApi(): array {
