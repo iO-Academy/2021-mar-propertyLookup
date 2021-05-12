051ed9f84e3a07ec41931305023d4cde6c2c3977
Isabelle Cochrane <isabelle_cochrane@hotmail.co.uk>
2021-05-12 10:21:24 +0100

    type hinting and docblocks for dbpopulater

--- src/Classes/DBPopulator.php
+++ src/Classes/DBPopulator.php
@@ -2,18 +2,22 @@
 namespace ListingsApp\Classes;
 class DBPopulator {
     private object $listingAPI;
-    private $dbConnection;
-    private $listings;
+    private object $dbConnection;
+    private array $listings;
     public function __construct($listingAPI, $db) {
         $this->listingAPI = $listingAPI;
         $this->dbConnection = $db;
         $this->populateDBAllTables();
     }
-    private function populateDBListingsTable() {
+
+    /**
+     * fills listings table in listings-feed db
+     */
+    private function populateDBListingsTable(): void {
         $this->emptyDB('listings');
         $this->listings = $this->listingAPI->getListingsFromApi();
         foreach($this->listings as $listing) {
-            $query = $this->dbConnection->prepare('INSERT INTO `listings` (`AGENT_REF`, `ADDRESS_1`, `ADDRESS_2`, `TOWN`, `POSTCODE`, `DESCRIPTION`, `BEDROOMS`, `PRICE`, `IMAGE`, `TYPE`, `STATUS`) VALUES (:agent_ref, :address_1, :address_2, :town, :postcode, :description, :bedrooms, :price, :image, :type, :status);');
+            $query = $this->dbConnection->prepare('INSERT INTO `listings` (`agent_ref`, `address_1`, `address_2`, `town`, `postcode`, `description`, `bedrooms`, `price`, `image`, `type`, `status`) VALUES (:agent_ref, :address_1, :address_2, :town, :postcode, :description, :bedrooms, :price, :image, :type, :status);');
             $query->bindParam(':agent_ref', $listing->AGENT_REF);
             $query->bindParam(':address_1', $listing->ADDRESS_1);
             $query->bindParam(':address_2', $listing->ADDRESS_2);
@@ -29,33 +33,47 @@ class DBPopulator {
         }
     }
 
-    private function populateDBTypesTable() {
+    /**
+     * fills types tables in listings-feed db
+     */
+    private function populateDBTypesTable(): void {
         $this->emptyDB('types');
         $this->listings = $this->listingAPI->getTypesFromApi();
         foreach ($this->listings as $listing) {
-            $query = $this->dbConnection->prepare('INSERT INTO `types` (`TYPE_NAME`) VALUES (:type_name);');
+            $query = $this->dbConnection->prepare('INSERT INTO `types` (`type_name`) VALUES (:type_name);');
             $query->bindParam(':type_name', $listing->TYPE_NAME);
             $query->execute();
         }
     }
 
-    private function populateDBStatusesTable() {
+    /**
+     * fills statuses tables in listings-feed db
+     */
+    private function populateDBStatusesTable(): void {
         $this->emptyDB('statuses');
         $this->listings = $this->listingAPI->getStatusesFromApi();
         foreach ($this->listings as $listing) {
-            $query = $this->dbConnection->prepare('INSERT INTO `statuses` (`STATUS_NAME`) VALUES (:status_name);');
+            $query = $this->dbConnection->prepare('INSERT INTO `statuses` (`status_name`) VALUES (:status_name);');
             $query->bindParam(':status_name', $listing->STATUS_NAME);
             $query->execute();
         }
     }
 
-    private function emptyDB($tableName) {
+    /**
+     * empties each table (called before it repopulates them)
+     * @param $tableName
+     *
+     */
+    private function emptyDB($tableName): void {
       $emptyTableQuery = "TRUNCATE TABLE `$tableName`";
       $statement = $this->dbConnection->prepare($emptyTableQuery);
       $statement->execute();
     }
 
-    public function populateDBAllTables() {
+    /**
+     * populates all tables 
+     */
+    public function populateDBAllTables(): void {
     $this->populateDBListingsTable();
     $this->populateDBStatusesTable();
     $this->populateDBTypesTable();
