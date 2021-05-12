70c49a493a201cf8db08035e43ca28ace2ae546e
Nick <nickclifford@gmail.com>
2021-05-12 13:37:42 +0100

    inserted new dropdown

--- index.php
+++ index.php
@@ -1,12 +1,6 @@
 <?php
-use ListingsApp\Classes\ListingAPI;
+use ListingsApp\Classes\Example;
 require_once 'vendor/autoload.php';
-
-$trial = new ListingAPI();
-var_dump($trial->getStatusesFromApi());
-var_dump($trial->getTypesFromApi());
-var_dump($trial->getListingsFromApi());
-
 ?>
 
 <!DOCTYPE html>
@@ -14,32 +8,50 @@ var_dump($trial->getListingsFromApi());
 <head>
     <meta charset="UTF-8">
     <link rel="normalize" href="app/css/normalize.css"/>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="app/css/style.css"/>
     <title>Solid Properties</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-    <div class="header">
-        <div class="basicNavbar">
-            <div class="basicNavbarButtons">
-                <a href="index.php?type=all"><button class="allBtn navButtons" >All</button></a>
-                <a href="index.php?type=sales"><button class="salesBtn navButtons">Sales</button></a>
-                <a href="index.php?type=lettings"><button class="lettingsBtn navButtons">Lettings</button></a>
-            </div>
-        </div>
-        <div class="jumbotron">
-            <div class="overlay">
-                <h1>SOLID</h1>
-                <h2>Properties</h2>
-            </div>
-        </div>
-        <div class="boostrapNavbar">
-
+<div class="header">
+    <div class="basicNavbar">
+        <div class="basicNavbarButtons">
+            <a href="index.php?type=all"><button class="allBtn navButtons" >All</button></a>
+            <a href="index.php?type=sales"><button class="salesBtn navButtons">Sales</button></a>
+            <a href="index.php?type=lettings"><button class="lettingsBtn navButtons">Lettings</button></a>
         </div>
     </div>
-    <div class="pageContent">
-
+    <div class="jumbotron">
+        <div class="overlay">
+            <h1>SOLID</h1>
+            <h2>Properties</h2>
+        </div>
     </div>
+    <div class="boostrapNavbar">
+        <div class="bootstrapTable">
+            <div class="dropdown show">
+                <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Min Price
+                </a>
+
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                    <a class="dropdown-item" href="https://getbootstrap.com/docs/4.0/components/dropdowns/">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+            </div>
+            <h3>Min Price</h3>
+            <h3>Max Price</h3>
+            <h3>Bedrooms</h3>
+        </div>
+    </div>
+</div>
+<div class="pageContent">
+
+</div>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </body>
 </html>
-
