19eab5ec23c01250b4f88d88cf0cc6769e5a1709
Isabelle Cochrane <isabelle_cochrane@hotmail.co.uk>
2021-05-12 13:44:12 +0100

    combined listing hydrator class

--- src/Classes/ListingHydrator.php
+++ src/Classes/ListingHydrator.php
@@ -1,9 +1,21 @@
 <?php
+
+
 namespace ListingsApp\Classes;
+
+
 use PDO;
 
 class ListingHydrator
 {
+    public function getListing(PDO $db, string $agent_ref): Listing
+    {
+        $query= $db->prepare("SELECT `AGENT_REF` AS 'agent_ref', `ADDRESS_1` AS 'address_1', `ADDRESS_2` AS 'address_2', `TOWN` AS 'town', `POSTCODE` AS 'postcode', `DESCRIPTION` AS 'description', `BEDROOMS` AS 'bedrooms', `PRICE` AS 'price', `IMAGE` AS 'image', `TYPE` AS 'type', `STATUS` AS 'status' FROM `listings` WHERE `AGENT_REF` = 'CSL123_100259';");
+        $query->setFetchMode(PDO::FETCH_CLASS, Listing::class);
+        $query->execute();
+        return $query->fetch();
+    }
+
     public static function getAllListings(PDO $db)
     {
         $query = $db->prepare("SELECT `agent_ref`, `address_1`, `address_2`, `town`, `postcode`, `description`, `bedrooms`, `price`, `image`, `type`, `status` FROM `listings`;");
