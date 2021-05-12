214c881271636bc809980b377c8ad5487331fcca
Isabelle Cochrane <isabelle_cochrane@hotmail.co.uk>
2021-05-12 14:35:02 +0100

    look at that bootstrap ooo

--- index.php
+++ index.php
@@ -11,6 +11,7 @@ use ListingsApp\Classes\ListingWindow;
     <meta charset="UTF-8">
     <link rel="normalize" href="app/css/normalize.css"/>
     <link rel="stylesheet" href="app/css/style.css"/>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <title>Solid Properties</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
@@ -38,10 +39,6 @@ use ListingsApp\Classes\ListingWindow;
         $db = new PDO('mysql:host=db;dbname=listings-feed', 'root', 'password');
         $listings = ListingHydrator::getAllListings($db);
         echo ListingWindow::displayListings($listings);
-        echo '<pre>';
-        var_dump($listings);
-        echo '</pre>';
-
         ?>
     </div>
 </body>
--- src/Classes/ListingWindow.php
+++ src/Classes/ListingWindow.php
@@ -43,24 +43,45 @@ class ListingWindow
     {
         $result = '';
         foreach($listings as $listing) {
-            $result .= '<img src ="https://dev.io-academy.uk/resources/property-feed/images/';
+            $result .= "<div class='listingCard card' style='width: 18rem;'>";
+            $result .= '<img class="card-img-top" alt="Image of property" src="https://dev.io-academy.uk/resources/property-feed/images/';
             $result .= $listing->getImage();
             $result .= '"/>';
-            $result .= '<div class= "infoDisplay">';
-            $result .= '<p>';
+            $result .= '<div class= "infoDisplay card-body">';
+            $result .= '<h5 class="card-title">';
             $result .= $listing->getAddress2();
-            $result .= '</p>';
-            $result .= '<p>';
+            $result .= '</h5>';
+            $result .= '<ul class="list-group list-group-flush">';
+            $result .= ' <li class="list-group-item">';
             $result .= $listing->getBedrooms();
             $result .= ' Bedrooms';
-            $result .= '</p>';
-            $result .= '<p>';
+            $result .= '</li>';
+            $result .= ' <li class="list-group-item">';
             $result .= $listing->getStatus();
-            $result .= '</p>';
-            $result .= '<p class="priceDisplay">';
+            $result .= '</li>';
+            $result .= '<li class="priceDisplay list-group-item">';
             $result .= number_format($listing->getPrice());
-            $result .= '</p>';
+            $result .= '</li>';
+            $result .= '</div>';
+            $result .= '</div>';
         }
         return $result;
     }
 }
+?>
+<!--<div class="card" style="width: 18rem;">-->
+<!--  <img class="card-img-top" src="..." alt="Card image cap">-->
+<!--  <div class="card-body">-->
+<!--    <h5 class="card-title">Card title</h5>-->
+<!--    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>-->
+<!--  </div>-->
+<!--  <ul class="list-group list-group-flush">-->
+<!--    <li class="list-group-item">Cras justo odio</li>-->
+<!--    <li class="list-group-item">Dapibus ac facilisis in</li>-->
+<!--    <li class="list-group-item">Vestibulum at eros</li>-->
+<!--  </ul>-->
+<!--  <div class="card-body">-->
+<!--    <a href="#" class="card-link">Card link</a>-->
+<!--    <a href="#" class="card-link">Another link</a>-->
+<!--  </div>-->
+<!--</div>'-->
