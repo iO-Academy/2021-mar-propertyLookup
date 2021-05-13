332c2ced43ce922fc03c4ac32da58476ab2db9c7
Sobia Furqan <sobiafurqan@hotmail.com>
2021-05-13 10:57:29 +0100

    adding in unit testing

---
+++ tests/Classes/ListingsWindowTest.php
@@ -0,0 +1,64 @@
+<?php
+
+use ListingsApp\Classes\ListingsWindow;
+
+use PHPUnit\Framework\TestCase;
+
+require_once '../../src/Classes/Listing.php';
+require_once '../../src/Classes/ListingsWindow.php';
+
+/**
+ * Class ListingWindowTest
+ */
+class ListingsWindowTest extends TestCase {
+    /**
+     * create mock of Listing class
+     * mock the Listing methods needed for ListingsWindow::displayListings()
+     * assert that the actual return value matches the expected return value
+     */
+    public function testDisplayListings_success()
+    {
+        $listingMock = $this->createMock(Listing::class);
+        $listingMock->expects($this->once())
+            ->method('getImage')
+            ->willReturn('CSL123_100327_IMG_00.JPG');
+        $listingMock->expects($this->once())
+            ->method('getAddress2')
+            ->willReturn('Plough Hill Road');
+        $listingMock->expects($this->once())
+            ->method('getBedrooms')
+            ->willReturn('6');
+        $listingMock->expects($this->once())
+            ->method('getStatus')
+            ->willReturn('For Sale');
+        $listingMock->expects($this->once())
+            ->method('getPrice')
+            ->willReturn('355000');
+        $listingMock->expects($this->once())
+            ->method('getAgentRef')
+            ->willReturn('CSL123_100259');
+
+
+        $result= ListingsWindow::displayListings($listingMock);
+        $expected = '<img src ="https://dev.io-academy.uk/resources/property-feed/images/CSL123_100327_IMG_00.JPG"/><div class= "infoDisplay"><p>Plough Hill Road</p><p>6 Bedrooms </p><p>For Sale</p><p class="priceDisplay">355,000</p><p>CSL123_100259</p></div><div class="descriptionDisplay"><h3>Description</h3><p>This is a rare opportunity...</p></div>';
+        $this->assertEquals($result, $expected);
+    }
+
+    /**
+     * test the result of passing a string to ListingWindow::displayListing()
+     */
+    public function testDisplayListing_failure1()
+    {
+        $this->expectException(Error::class);
+        ListingWindow::displayListing('$listingMock');
+    }
+
+    /**
+     * test the result of passing an array of string/s to ListingWindow::displayListing()
+     */
+    public function testDisplayListing_failure2()
+    {
+        $this->expectException(Error::class);
+        ListingWindow::displayListing(['$listingMock']);
+    }
+}
