88c763ad1e2f99ff08b3e68f9ca1d01da575dc25
Nathaniel Payne <somersetnathaniel@gmail.com>
2021-05-13 14:16:47 +0100

    removed extraneous php Classes and refactored existing code

--- src/Classes/ListingHydrator.php
+++ src/Classes/ListingHydrator.php
@@ -27,4 +27,16 @@ class ListingHydrator
         $query->execute();
         return $query->fetch();
     }
-};
+
+    /**
+     * @param PDO $db
+     * @return array of Listing objects
+     */
+    public static function getAllListings(PDO $db)
+    {
+        $query = $db->prepare("SELECT `agent_ref`, `address_1`, `address_2`, `town`, `postcode`, `description`, `bedrooms`, `price`, `image`, `type`, `status` FROM `listings`;");
+        $query->execute();
+        $query->setFetchMode(PDO::FETCH_CLASS, Listing::class);
+        return $query->fetchAll();
+    }
+}
